d98b9c1a34a9c3fe0b2f1670edfaf130a074e075
Chris Ward <chris.ward@northcoders.com>
2023-11-02 14:53:01 +0000

    Update useReview.ts

--- src/hooks/useReview.ts
+++ src/hooks/useReview.ts
@@ -16,9 +16,9 @@ export const useReview = () => {
   return { heroics: reviews, waitAMo, fail };
 };
 
-export interface Review {
+export interface Reviews {
   image: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80";
-  reviews: Array<ReviewProps>;
+  testimonials: Array<ReviewProps>;
 }
 
 interface ReviewProps {
